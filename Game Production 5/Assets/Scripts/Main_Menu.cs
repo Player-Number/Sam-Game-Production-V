c1963eeb4824775dd3540807ac8ea5263a946aca
Player-Number <101483439@georgebrown.ca>
2025-09-07 16:05:03 -0400

    added a basic Main Menu scene
    it has:
    - Title
    - Play button
    - basic "How to Play" text

---
+++ Game Production 5/Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void To_Game()
+    {
+        SceneManager.LoadScene("Game_Scene");
+    }
+}
