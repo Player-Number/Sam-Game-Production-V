b8e28adb1276076ee26f774c5bdcd1526bd8b4e1
Player-Number <101483439@georgebrown.ca>
2025-09-12 17:27:25 -0400

    cleaned up code - put code in "Update" into functions

--- Game Production 5/Assets/Scripts/Player.cs
+++ Game Production 5/Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -8,12 +9,15 @@ public class Player : MonoBehaviour
     [SerializeField] TMP_Text Dash_cool_Text;
     [SerializeField] Camera Cam;
     [SerializeField] GameObject Pause_Menu;
+    [SerializeField] InputActionAsset input_actions;
+
     public GameObject Door;
 
     Rigidbody rb;
 
     public float Collectable_remaining = 2;
-    float move_speed = 500;
+    float rb_move_speed = 500;
+    //float move_speed = 10;
     float Timer = 0;
     float horizontal_move_cap = 4;
     float vertical_move_cap = 5;
@@ -27,6 +31,9 @@ public class Player : MonoBehaviour
 
     Vector3 new_room_trigger_pos;
 
+    //InputAction move_input;
+    //Vector2 dir;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -34,28 +41,45 @@ public class Player : MonoBehaviour
         Collectable_Text.text = "Collectable Remaining: " + (Collectable_remaining);
         Dash_cool_Text.text = "Dash Cooldown: " + dash_cool.ToString("F0");
         Time.timeScale = 1.0f;
+        //move_input = input_actions.FindAction("Move");
         //Cursor.visible = false;
         //Cursor.lockState = CursorLockMode.Locked;
     }
 
     void Update()
     {
+        Move();
+        Other_Actions();
+        Move_Cap();
+
+        if (move_door)
+        {
+            Door.transform.position += Vector3.up * Time.deltaTime * 2;
+            if (Door.transform.position.y >= 6.5f)
+            {
+                move_door = false;
+                //Door.gameObject.SetActive(false);
+            }
+        }
+
+        Timer += Time.deltaTime;
+        Timer_Text.text = Timer.ToString("F2");
+    }
+
+    private void Move()
+    {
         if (Input.GetKey(KeyCode.W))
-        {
-            rb.AddForce(rb.transform.forward * Time.deltaTime * move_speed);
-        }
+            rb.AddForce(rb.transform.forward * Time.deltaTime * rb_move_speed);
         if (Input.GetKey(KeyCode.A))
-        {
-            rb.AddForce(-rb.transform.right * Time.deltaTime * move_speed);
-        }
+            rb.AddForce(-rb.transform.right * Time.deltaTime * rb_move_speed);
         if (Input.GetKey(KeyCode.S))
-        {
-            rb.AddForce(-rb.transform.forward * Time.deltaTime * move_speed);
-        }
+            rb.AddForce(-rb.transform.forward * Time.deltaTime * rb_move_speed);
         if (Input.GetKey(KeyCode.D))
-        {
-            rb.AddForce(rb.transform.right * Time.deltaTime * move_speed);
-        }
+            rb.AddForce(rb.transform.right * Time.deltaTime * rb_move_speed);
+    }
+
+    private void Other_Actions()
+    {
         if (Input.GetKeyDown(KeyCode.R))
         {
             transform.position = new_room_trigger_pos;
@@ -86,60 +110,32 @@ public class Player : MonoBehaviour
             rb.AddForce(Vector3.up * 500);
             //is_grounded = false;
         }
-        if (rb.linearVelocity.y >= vertical_move_cap)
-        {
-            rb.linearVelocity = new(rb.linearVelocity.x, vertical_move_cap, rb.linearVelocity.z);
-        }
-        //if (rb.linearVelocity.y <= -vertical_move_cap)
-        //{
-        //    rb.linearVelocity = new(rb.linearVelocity.x, -vertical_move_cap, rb.linearVelocity.z);
-        //}
-        if (rb.linearVelocity.x >= horizontal_move_cap)
-        {
-            rb.linearVelocity = new(horizontal_move_cap, rb.linearVelocity.y, rb.linearVelocity.z);
-        }
-        if (rb.linearVelocity.x <= -horizontal_move_cap)
-        {
-            rb.linearVelocity = new(-horizontal_move_cap, rb.linearVelocity.y, rb.linearVelocity.z);
-        }
-        if (rb.linearVelocity.z >= horizontal_move_cap)
-        {
-            rb.linearVelocity = new(rb.linearVelocity.x, rb.linearVelocity.y, horizontal_move_cap);
-        }
-        if (rb.linearVelocity.z <= -horizontal_move_cap)
-        {
-            rb.linearVelocity = new(rb.linearVelocity.x, rb.linearVelocity.y, -horizontal_move_cap);
-        }
-
-
         if (rb.linearVelocity.y == 0)
             is_grounded = true;
         else
             is_grounded = false;
 
-        //if (mud_timer > 0)
-        //{
-        //    mud_timer -= Time.deltaTime;
-        //    is_grounded = false;
-        //    //if (mud_timer <= 0)
-        //    //{
-        //    //    mud_timer = 1;
-        //    //    can_jump = true;
-        //    //} 
-        //}
-
-        if (move_door)
+        //Dev only
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Door.transform.position += Vector3.up * Time.deltaTime * 2;
-            if (Door.transform.position.y >= 6.5f)
-            {
-                move_door = false;
-                //Door.gameObject.SetActive(false);
-            }
+            transform.position = (transform.position + Cam.gameObject.transform.forward * 5);
         }
+    }
 
-        Timer += Time.deltaTime;
-        Timer_Text.text = Timer.ToString("F2");
+    private void Move_Cap()
+    {
+        if (rb.linearVelocity.y >= vertical_move_cap)
+            rb.linearVelocity = new(rb.linearVelocity.x, vertical_move_cap, rb.linearVelocity.z);
+        //if (rb.linearVelocity.y <= -vertical_move_cap)
+        //    rb.linearVelocity = new(rb.linearVelocity.x, -vertical_move_cap, rb.linearVelocity.z);
+        if (rb.linearVelocity.x >= horizontal_move_cap)
+            rb.linearVelocity = new(horizontal_move_cap, rb.linearVelocity.y, rb.linearVelocity.z);
+        if (rb.linearVelocity.x <= -horizontal_move_cap)
+            rb.linearVelocity = new(-horizontal_move_cap, rb.linearVelocity.y, rb.linearVelocity.z);
+        if (rb.linearVelocity.z >= horizontal_move_cap)
+            rb.linearVelocity = new(rb.linearVelocity.x, rb.linearVelocity.y, horizontal_move_cap);
+        if (rb.linearVelocity.z <= -horizontal_move_cap)
+            rb.linearVelocity = new(rb.linearVelocity.x, rb.linearVelocity.y, -horizontal_move_cap);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -177,7 +173,7 @@ public class Player : MonoBehaviour
         }
         else if (other.gameObject.name == "Speed_Area")
         {
-            move_speed *= 2;
+            rb_move_speed *= 2;
         }
     }
 
@@ -185,7 +181,7 @@ public class Player : MonoBehaviour
     {
         if (other.gameObject.name == "Speed_Area")
         {
-            move_speed /= 2;
+            rb_move_speed /= 2;
         }
         //if (other.gameObject.name == "Mud")
         //{
@@ -200,4 +196,33 @@ public class Player : MonoBehaviour
     //        is_grounded = true;
     //    }
     //}
+
+    //private void New_Input_Move()
+    //{
+    //    Debug.Log(move_input.ReadValue<Vector2>() + "move_input.ReadValue<Vector2>()");
+    //    dir = move_input.ReadValue<Vector2>();
+    //    Vector2 move_amount = dir * move_speed * Time.deltaTime;
+    //    transform.position = new Vector3(transform.position.x + move_amount.x, transform.position.y, transform.position.z + move_amount.y);
+
+    //}
+
+    private void Unused()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = (rb.transform.forward * Time.deltaTime);
+            transform.position = Vector3.up * Time.deltaTime;
+        }
+
+        //if (mud_timer > 0)
+        //{
+        //    mud_timer -= Time.deltaTime;
+        //    is_grounded = false;
+        //    //if (mud_timer <= 0)
+        //    //{
+        //    //    mud_timer = 1;
+        //    //    can_jump = true;
+        //    //} 
+        //}
+    }
 }
